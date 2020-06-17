51d78d02e04f4d45e505fa965dc409c195ef0c56
Vitor Bertti <berttivitor@gmail.com>
2020-06-17 19:02:59 -0300

    Created converter

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,86 +1,93 @@
 <template>
-   <div class="hello">
-      <h1>{{ msg }}</h1>
-      <div>
-         <input type="file" accept="application/JSON" @change="loadTextFromFile" />
-         <br /><br />
-         <a id="lnkDwnldLnk" hidden="hidden">Download</a>
-      </div>
-   </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <input type="file" accept="application/JSON" @change="loadTextFromFile" />
+      <br />
+      <br />
+      <a id="lnkDwnldLnk" hidden="hidden">Download</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class Converter extends Vue {
-   @Prop() private msg!: string;
+  @Prop() private msg!: string;
 
-   loadTextFromFile(event: any) {
-      const file = event.target.files[0];
-      const link = document.querySelector('#lnkDwnldLnk');
-      const json: object[] = [{}];
+  loadTextFromFile(event: any) {
+    const file = event.target.files[0];
+    const link = document.querySelector("#lnkDwnldLnk");
 
-      if (file) {
-         const reader = new FileReader();
-         reader.onload = (e: any) => {
-            const loadedFile = String(reader.result);
-            const jsonFile = JSON.parse(loadedFile);
-            json.shift();
+    if (file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+          const loadedFile = String(reader.result);
+          const jsonFile = JSON.parse(loadedFile);
+          resolve(jsonFile);
+        };
+        reader.readAsText(file);
+      }).then(json => {
+        const filename = file.name.split(".");
 
-            jsonFile.map((obj: any) => {
-               json.push(obj);
-            });
-         };
-         reader.readAsText(file);
+        if (link) {
+          link.removeAttribute("hidden");
+        }
 
-         const filename = file.name.split('.');
-         if (link) {
-            link.removeAttribute('hidden');
-         }
-
-         this.convertToCSV(json, filename[0], link as Element);
-      } else {
-         if (link) {
-            link.setAttribute('hidden', 'true');
-         }
-      }
-   }
-
-   convertToCSV(json: any, filename: string, link: Element) {
-      const items = json;
-      const replacer = (key: string, value: string) => (value === null ? '' : value);
-      const header = items ? Object.keys(items[0]) : [];
-
-      let csv = items ? items.map((row: any) => header.map((fieldName) => JSON.stringify(row[fieldName], replacer)).join(',')) : [];
-      csv.unshift(header.join(','));
-      csv = csv.join('\r\n');
-
-      const blob = new Blob([csv], { type: 'text/csv' });
-      const csvUrl = window.webkitURL.createObjectURL(blob);
-      const filenameConverted = `${filename}.csv`;
-
+        this.convertToCSV(json, filename[0], link as Element);
+      });
+    } else {
       if (link) {
-         link.setAttribute('download', filenameConverted);
-         link.setAttribute('href', csvUrl);
+        link.setAttribute("hidden", "true");
       }
-   }
+    }
+  }
+
+  convertToCSV(json: any, filename: string, link: Element) {
+    const items = json;
+    const replacer = (key: string, value: string) =>
+      value === null ? "" : value;
+    const header = items ? Object.keys(items[0]) : [];
+
+    let csv = items
+      ? items.map((row: any) =>
+          header
+            .map(fieldName => JSON.stringify(row[fieldName], replacer))
+            .join(",")
+        )
+      : [];
+    csv.unshift(header.join(","));
+    csv = csv.join("\r\n");
+
+    const blob = new Blob([csv], { type: "text/csv" });
+    const csvUrl = window.webkitURL.createObjectURL(blob);
+    const filenameConverted = `${filename}.csv`;
+
+    if (link) {
+      link.setAttribute("download", filenameConverted);
+      link.setAttribute("href", csvUrl);
+    }
+  }
 }
 </script>
 
 <style scoped>
 h3 {
-   margin: 40px 0 0;
+  margin: 40px 0 0;
 }
 ul {
-   list-style-type: none;
-   padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-   display: inline-block;
-   margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-   color: #42b983;
+  color: #42b983;
 }
 </style>
